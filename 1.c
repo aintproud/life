a50ef271f708c7fc60fd7a2b486a9cb66ceabc6d
Your Name <you@example.com>
2023-10-24 22:43:56 +1000

    kkjnlwfd

--- 1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("\033[0;31m"); // Set color to red
-    printf("This text is in red color.\n");
-    printf("\033[0m"); // Reset color to default
-
-    return 0;
-}
